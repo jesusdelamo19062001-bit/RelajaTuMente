00ba66076aea78300c37409b05b67ebedcafe0e1
jesusdelamo19062001-bit <jesusdelamo19062001@gmail.com>
2026-05-06 21:22:48 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'rtm-v1';
+const ASSETS = [
+  '/RelajaTuMente/',
+  '/RelajaTuMente/index.html',
+  '/RelajaTuMente/manifest.json',
+  '/RelajaTuMente/icon-192.png',
+  '/RelajaTuMente/icon-512.png',
+  '/RelajaTuMente/icon-maskable-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
